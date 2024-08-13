e6405c6ecdc595c96d77ec3e4f7f45de617aa326
saeedyussif663 <saeedyussif663@gmail.com>
2024-08-13 22:53:06 +0000

    refactored to async/await && fixed update bug

--- src/controllers/blogController.ts
+++ src/controllers/blogController.ts
@@ -1,50 +1,63 @@
-import { Request, Response } from "express";
-import Blog from "../models/blogModel";
+import { Request, Response } from 'express';
+import Blog from '../models/blogModel';
 
 // fetch all blogs
-export function fetchAllBlogs(req: Request, res: Response) {
-  Blog.find({})
-    .then((blogs) => res.status(200).json(blogs))
-    .catch((error) => res.json({ message: "An error occurred" }));
+export async function fetchAllBlogs(req: Request, res: Response) {
+  try {
+    const blogs = await Blog.find({});
+    return res.status(200).json(blogs);
+  } catch (error) {
+    return res.json({ message: 'An error occurred' });
+  }
 }
 
 // fetch a single blog
-export function fetchSingleBlog(req: Request, res: Response) {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((blog) => res.status(200).json(blog))
-    .catch((err) => res.status(404).json({ message: "Blog not found" }));
+export async function fetchSingleBlog(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const blog = await Blog.findById(id);
+    return res.status(200).json(blog);
+  } catch (error) {
+    return res.status(404).json({ message: 'Blog not found' });
+  }
 }
 
 // create blog
-export function createBlog(req: Request, res: Response) {
-  Blog.create(req.body)
-    .then((blog) => res.status(201).json(blog))
-    .then((error) =>
-      res.status(400).json({ message: "An error occured creating blog" })
-    );
+export async function createBlog(req: Request, res: Response) {
+  try {
+    const blog = await Blog.create(req.body);
+    return res
+      .status(201)
+      .json({ blog: blog, message: 'Created blog successfully' });
+  } catch (error) {
+    return res.status(400).json({ message: 'An error occured creating blog' });
+  }
 }
 
 // edit blog
-export function editBlog(req: Request, res: Response) {
+export async function editBlog(req: Request, res: Response) {
   const id = req.params.id;
-  Blog.findByIdAndUpdate(id)
-    .then((resolve) =>
-      res.status(201).json({ message: "updated successfully" })
-    )
-    .catch((error) =>
-      res.status(404).json({ message: "An error occurred updating your blog" })
-    );
+  try {
+    const blog = Blog.findByIdAndUpdate(id, req.body);
+    return res
+      .status(201)
+      .json({ blog: blog, message: 'updated blog successfully' });
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ message: 'An error occurred updating your blog' });
+  }
 }
 
 // delete blog
-export function deleteBlog(req: Request, res: Response) {
+export async function deleteBlog(req: Request, res: Response) {
   const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((resolve) =>
-      res.status(200).json({ message: "Deleted blog successfully" })
-    )
-    .catch((error) =>
-      res.status(404).json({ message: "An error occurred deleting your blog" })
-    );
+  try {
+    await Blog.findByIdAndDelete(id);
+    return res.status(200).json({ message: 'Deleted blog successfully' });
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ message: 'An error occurred deleting your blog' });
+  }
 }
